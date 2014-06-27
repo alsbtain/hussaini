8c3a8faae21598ac01bda3b1dce27f294515d8da
alsbtain <info@alsbtain.net>
2014-06-27 01:40:10 -0700

    updated js

--- www/js/sb.js
+++ www/js/sb.js
@@ -17,12 +17,18 @@ var elmnt = document.getElementById(id);
   }
 }
 
-function labelThis(id){
+function labelThis(id,day){
 	if(localStorage.getItem('label' + id)=='1'){
 		localStorage.removeItem('label' + id);
+		var myarr = localStorage.getItem(day).split(";");
+		var index;
+		var myNewArray = [];
+		for (index = 0; index < myarr.length; ++index) {if(myarr[index]>0 && myarr[index] != id){myNewArray[myNewArray.length]=myarr[index];}}
+		localStorage.setItem(day, myNewArray.join(';'));
 		document.getElementById('label' + id).src='images/label_32_bw.png';
 	}else{
 		localStorage.setItem('label' + id, '1');
+		localStorage.setItem(day, localStorage.getItem(day) + ';' + id);
 		document.getElementById('label' + id).src='images/label_32.png';
 	}
 }
