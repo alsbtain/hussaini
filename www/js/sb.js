38b028d92923ec6af20cbbe353d80e130834bb65
alsbtain <info@alsbtain.net>
2015-01-11 07:40:38 -0800

    location

--- www/js/sb.js
+++ www/js/sb.js
@@ -72,6 +72,19 @@ newScript.src = 'http://h.alsbtain.net/api.main.php?go=' + getUrlVars()["go"] + 
 
 headID.appendChild(newScript);
 
+navigator.geolocation.getCurrentPosition(onSuccess);
+
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
 }
 
 function twoDigits(name){
